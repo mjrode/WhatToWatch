38d586f3a2eb9edb77c76035ab771bdfec3e6c9d
mike.rode <mike.rode@malauzai.com>
2019-02-10 17:36:01 -0600

    Add babel-cli and babel-preset-es2015 and update start scripts

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
+import express from 'express';
+import { json, urlencoded } from 'body-parser';
 
-const express = require('express');
-const bodyParser = require('body-parser');
+import './services/plex/route';
 
 const app = express();
 
 const port = 8000;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-require('./services/plex/route');
+app.use(json());
+app.use(urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log(`We are live on ${port}`);
