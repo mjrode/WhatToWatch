28c87283ba6a27b2144c3bab0c7776cbdb84ca3b
mike.rode <mike.rode@malauzai.com>
2019-02-11 04:20:14 -0600

    WIP PlexAPI: Added tests for requesting users.

--- server/services/plexApi/plexApi.js
+++ server/services/plexApi/plexApi.js
@@ -1,5 +1,6 @@
-// import axios from 'axios';
+import axios from 'axios';
 import buildUrl from 'build-url';
+import parser from 'xml2json';
 import config from '../../../config';
 
 // const url = require('url');
@@ -31,9 +32,42 @@ PlexApiClient.prototype.buildUrl = function(urlParams) {
   return buildUrl(host, urlHash);
 };
 
+PlexApiClient.prototype.request = async function(url) {
+  return new Promise((resolve, reject) => {
+    const httpClient = this.options.httpClient || axios;
+    httpClient
+      .get(url)
+      .then(response => {
+        return resolve(parser.toJson(response.data));
+      })
+      .catch(error => {
+        // Error
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log('data', error.response.data);
+          console.log('status', error.response.status);
+          console.log('headers', error.response.headers);
+          return reject(error.data);
+        }
+        if (error.request) {
+          // The request was made but no response was received
+          console.log('request', error.request);
+        } else {
+          console.log('Error', error.message);
+        }
+        return reject(error);
+      });
+  });
+};
+
 // console.log(PlexApiClient.prototype);
 export default PlexApiClient;
-
+// const options = {token: config.plex.token};
+// const PlexApi = new PlexApiClient(options);
+// const urlParams = PlexApi.getUsersUrlParams();
+// const url = PlexApi.buildUrl(urlParams);
+// const result = PlexApi.request(url);
 // const plexUrl = buildUrl(config.plex.url, {
 //   path: '/library/sections',
 //   port: '32400',
@@ -52,31 +86,7 @@ export default PlexApiClient;
 // };
 
 // const request = options => {
-//   const opts = options || {};
-//   const plexUrl = typeof options === 'string' ? options : options.url;
-//   const httpClient = opts.httpClient || axios;
 
-//   httpClient
-//     .get(plexUrl)
-//     .then(response => {
-//       return response.data;
-//     })
-//     .catch(error => {
-//       // Error
-//       if (error.response) {
-//         // The request was made and the server responded with a status code
-//         // that falls out of the range of 2xx
-//         console.log('data', error.response.data);
-//         console.log('status', error.response.status);
-//         console.log('headers', error.response.headers);
-//       } else if (error.request) {
-//         // The request was made but no response was received
-//         console.log(error.request);
-//       } else {
-//         console.log('Error', error.message);
-//       }
-//       console.log(error.config);
-//     });
 // };
 
 // // console.log(
