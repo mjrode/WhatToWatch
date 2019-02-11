61659d1ab2b5ce10168e61f69268103afd091282
mike.rode <mike.rode@malauzai.com>
2019-02-10 18:31:46 -0600

    Add prettier, eslint, and husky with npm scripts.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "rules": {
-      'no-console': 0
-    }
+  "extends": [
+    "airbnb-base", 
+    "prettier"
+  ],
+  "rules": {
+    'no-console': 0,
+    'import/no-dynamic-require': 0,
+  }
 };
