d32aa3c6cf97f5ad932b495b81419842b6e35b26
mike.rode <mike.rode@malauzai.com>
2019-02-10 20:56:40 -0600

    Use babel preset node8 and force arrow functions.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,21 @@
 module.exports = {
-  "extends": [
-    "airbnb-base", 
-    "prettier"
-  ],
-  "rules": {
+  extends: ['airbnb-base', 'prettier'],
+  env: {
+    node: true,
+  },
+  plugins: ['prefer-arrow'],
+  rules: {
     'no-console': 0,
     'import/no-dynamic-require': 0,
-  }
+    'func-names': 0,
+    'prefer-arrow-callback': 0,
+    'prefer-arrow/prefer-arrow-functions': [
+      'warn',
+      {
+        disallowPrototype: true,
+        singleReturnOnly: false,
+        classPropertiesAllowed: false,
+      },
+    ],
+  },
 };
