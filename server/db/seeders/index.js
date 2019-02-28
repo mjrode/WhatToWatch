a24cc4df00b1546b1d3e6b977fc1624de67a22d5
mike.rode <mike.rode@malauzai.com>
2019-02-27 19:23:28 -0600

    Revert user auth logic.

--- server/db/seeders/index.js
+++ server/db/seeders/index.js
@@ -4,7 +4,6 @@ export default {
       firstName: 'Mike',
       lastName: 'Rode',
       email: 'mike@email.com',
-      password: 'password123',
     },
   ],
 };
--- server/routes/auth.js
+++
@@ -1,6 +0,0 @@
-import authController from '../controllers/auth.controller';
-
-module.exports = function(app) {
-  app.get('/signup', authController.signup);
-  app.get('/signin', authController.signin);
-};
