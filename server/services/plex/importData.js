d270031d86273475021593f7164020494dd0379f
mike.rode <mike.rode@malauzai.com>
2019-02-27 18:50:33 -0600

    Use Promise.map for importing sections.

--- server/services/plex/importData.js
+++ server/services/plex/importData.js
@@ -4,26 +4,24 @@ import models from '../../db/models';
 
 const importSections = async () => {
   const sections = await plexApi.getSections();
-  const sectionsDb = await createSections(sections);
-  return sectionsDb;
+  const dbSections = await createSections(sections);
+  return dbSections;
 };
 
 const createSections = sections => {
-  return Promise.try(() => {
-    return sections.forEach(section => {
-      models.PlexSection.upsert(
-        {
+  return Promise.map(sections, section => {
+    return models.PlexSection.upsert(
+      {
+        title: section.title,
+        type: section.type,
+        key: section.key,
+      },
+      {
+        where: {
           title: section.title,
-          type: section.type,
-          key: section.key,
         },
-        {
-          where: {
-            title: section.title,
-          },
-        },
-      );
-    });
+      },
+    );
   }).catch(err => {
     console.log(err);
   });
@@ -47,54 +45,61 @@ const importLibrary = async sectionId => {
   const libraryData = await plexApi.getLibraryDataBySection({
     sectionId,
   });
-  createLibrary(libraryData);
-  return libraryData;
+  const dbLibraryData = await createLibrary(libraryData);
+  console.log(dbLibraryData);
+  return dbLibraryData;
 };
 
 const updateLibrary = libraryData => {
-  libraryData.forEach(async data => {
-    await models.PlexLibrary.update(
-      {
-        title: data.title,
-        type: data.type,
-        views: data.globalViewCount,
-        rating_key: data.ratingKey,
-        metadata_path: data.key,
-        summary: data.summary,
-        rating: data.rating,
-        year: data.year,
-        genre: JSON.stringify(data.Genre),
-      },
-      {
-        where: {
+  return Promise.try(() => {
+    libraryData.forEach(data => {
+      models.PlexLibrary.update(
+        {
           title: data.title,
+          type: data.type,
+          views: data.globalViewCount,
+          rating_key: data.ratingKey,
+          metadata_path: data.key,
+          summary: data.summary,
+          rating: data.rating,
+          year: data.year,
+          genre: JSON.stringify(data.Genre),
         },
-      },
-    );
+        {
+          where: {
+            title: data.title,
+          },
+        },
+      );
+    });
+  }).catch(err => {
+    console.log(err);
   });
 };
 
 const createLibrary = libraryData => {
-  libraryData.forEach(async data => {
-    await models.PlexLibrary.upsert(
-      {
-        title: data.title,
-        type: data.type,
-        views: data.views,
-        rating_key: data.ratingKey,
-        metadata_path: data.key,
-        summary: data.summary,
-        rating: data.rating,
-        year: data.year,
-        genre: JSON.stringify(data.Genre),
-      },
-      {
-        where: {
+  return Promise.try(() => {
+    libraryData.forEach(data => {
+      models.PlexLibrary.upsert(
+        {
           title: data.title,
+          type: data.type,
+          views: data.views,
+          rating_key: data.ratingKey,
+          metadata_path: data.key,
+          summary: data.summary,
+          rating: data.rating,
+          year: data.year,
+          genre: JSON.stringify(data.Genre),
         },
-      },
-    );
-  });
+        {
+          where: {
+            title: data.title,
+          },
+        },
+      );
+    });
+  }).catch(err => console.log(err));
 };
 
 export default {importSections, importLibraries, importMostWatched};
--- test/server/services/plex/importData.test.js
+++ test/server/services/plex/importData.test.js
@@ -6,10 +6,9 @@ import models from '../../../../server/db/models';
 import { seed, truncate } from '../../../../server/db/scripts';
 import * as nocks from '../../../nocks';
 
-// // before(() => truncate('PlexSection'));
 describe('ImportData', () => {
   before(() => {
-    nocks.plexSections();
+    truncate('User');
     seed('User');
   });
   after(() => {
@@ -20,47 +19,34 @@ describe('ImportData', () => {
 
   describe('GET /plex/import/sections', () => {
     it('should find and store sections in the database', async () => {
-      const response = await importData.importSections();
-      console.log('import sec response', response);
+      nocks.plexSections();
+      await importData.importSections();
       const sections = await models.PlexSection.findAll();
-      console.log('Sections response', sections);
+      const movies = sections.filter(
+        data => data.dataValues.title === 'Movies',
+      );
+      movies[0].dataValues.title.should.eq('Movies');
+      movies[0].dataValues.type.should.eq('movie');
+      movies[0].dataValues.key.should.eq(2);
+
+      const tvShows = sections.filter(
+        data => data.dataValues.title === 'TV Shows',
+      );
+      tvShows[0].dataValues.title.should.eq('TV Shows');
+      tvShows[0].dataValues.type.should.eq('show');
+      tvShows[0].dataValues.key.should.eq(3);
+
       sections.should.be.length(2);
     });
   });
+
+  // describe('GET /plex/import/libraries', () => {
+  //   it('should find and store libraries in the database', async () => {
+  //     nocks.plexSections();
+  //     nocks.plexLibrary();
+  //     const response = await importData.importLibraries();
+  //     const library = await models.PlexLibrary.findAll();
+  //     library.should.be.length(52);
+  //   });
+  // });
 });
-
-// describe('createRecord', () => {
-//   const name = 'testCreateIM';
-//   const im = { name, description: 'new interface method' };
-//   let jwt;
-
-//   before((done) => {
-//     createToken('test')
-//       .then((token) => {
-//         jwt = token;
-//         done();
-//       });
-//   });
-
-//   after(() => Interfacemethod.destroy({ where: { name } }));
-
-//   it('creates record', (done) => {
-//     let responseRecord;
-//     chai.request(app)
-//       .post('/v1/interfacemethods')
-//       .set('Authorization', `Bearer ${jwt}`)
-//       .send(im)
-//       .then((res) => {
-//         expect(res).to.have.status(200);
-//         expect(res.body).to.have.property('record');
-//         responseRecord = res.body.record;
-//         return Interfacemethod.findByPk(responseRecord.id);
-//       })
-//       .then((dbRecord) => {
-//         expect(responseRecord).to.deep.equal(JSON.parse(JSON.stringify(dbRecord)));
-//         expect(im.name).to.equal(responseRecord.name);
-//         expect(im.description).to.equal(responseRecord.description);
-//         done();
-//       });
-//   });
-// });
