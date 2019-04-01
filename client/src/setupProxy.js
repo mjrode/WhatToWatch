6d7695e901dd549d9a759ba47b4cea59bb6326f4
mjrode <mike.rode@malauzai.com>
2019-03-31 19:10:24 -0500

    Typo

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require('http-proxy-middleware');
 
-module.exports = function(app) {
-  app.use(
+module.exports = function(server) {
+  server.use(
     proxy(['/plex/sections', '/auth/google'], {
       target: 'http://localhost:8080',
     }),
--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ export default () => {
     });
 
     if (process.env.NODE_ENV === 'production') {
-      app.use(express.static('client/build'));
+      server.use(express.static('client/build'));
       const path = require('path');
       app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
