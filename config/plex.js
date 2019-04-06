edf99386e20456f868b02f735377991aad43cfe3
mjrode <mike.rode@malauzai.com>
2019-04-06 03:34:12 -0500

    Heroku fix

--- config/plex.js
+++ config/plex.js
@@ -1,6 +1,6 @@
 const plexConfig = {
   ip: 'http://192.168.0.44',
-  plexUrl: 'https://plex.mjrflix.com',
+  plexUrl: process.env.PLEX_URL,
   plexApiUrl: 'https://plex.tv/api',
   token: process.env.PLEX_API_TOKEN,
 };
