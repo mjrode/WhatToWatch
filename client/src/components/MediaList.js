2f1fe305e7c40128e4ec372136d54d3f9cf9c8cb
mjrode <mike.rode@malauzai.com>
2019-04-06 02:50:58 -0500

    List view with tv shows

--- client/src/components/MediaList.js
+++ client/src/components/MediaList.js
@@ -6,32 +6,40 @@ import styles from './css';
 import axios from 'axios';
 
 class MediaList extends Component {
+  state = {tvShowList: []};
+
   componentDidMount() {
     this.getMostWatched();
   }
   getMostWatched = async params => {
     const res = await axios.get('/api/recommend/most-watched');
-    console.log(res);
-    return res;
+    this.setState({tvShowList: res.data});
   };
 
   render() {
-    const {classes} = this.props;
-    return (
-      <ul className="collection">
-        <li className="collection-item avatar">
-          <img src="images/yuna.jpg" alt="pic" className="circle" />
-          <span className="title">Title</span>
-          <p>
-            First Line <br />
-            Second Line
-          </p>
-          <a href="#!" className="secondary-content">
-            <i className="material-icons">grade</i>
-          </a>
-        </li>
-      </ul>
-    );
+    const mediaList = this.state.tvShowList.map(show => {
+      return (
+        <ul className="collection" key={show.title}>
+          <li className="collection-item avatar">
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${show.poster_path}`}
+              alt="pic"
+              className="circle"
+            />
+            <span className="title">{show.title}</span>
+            <p>
+              {show.summary}
+              <br />
+              Views: {show.views}
+            </p>
+            <a href="#!" className="secondary-content">
+              <i className="material-icons">grade</i>
+            </a>
+          </li>
+        </ul>
+      );
+    });
+    return <div>{mediaList}</div>;
   }
 }
 
