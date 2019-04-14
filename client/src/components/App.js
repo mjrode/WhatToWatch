f865fcb7e6e057d9d813457c93e226ab380a50a7
mjrode <mike.rode@malauzai.com>
2019-04-13 21:28:32 -0500

    Add GA

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import {connect} from 'react-redux';
 import * as actions from '../actions';
+import ReactGA from 'react-ga';
 
 import Header from './Header';
 import Hero from './Hero';
@@ -9,6 +10,10 @@ import Plex from './plex/Plex';
 import SimilarList from './SimilarList';
 import PopularList from './PopularList';
 
+function initializeReactGA() {
+  ReactGA.initialize('UA-138294820-1');
+  ReactGA.pageview('/homepage');
+}
 class App extends Component {
   componentDidMount() {
     this.props.fetchUser();
